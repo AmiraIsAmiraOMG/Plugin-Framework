ea49d530f75fb85c2d80d03de9f6fe6e3bd21b2c
Wild Mastery Entertainment <167897093+AmiraIsAmiraOMG@users.noreply.github.com>
2024-12-22 20:25:40 +0000

    First Commit
    This was made at the time of NEXUS UTILITY BETA PRE RELEASE 1

---
+++ MyPluginFramework/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PluginSystem {
+
+    /// Defines the interface that all plugins must implement.
+    public interface IPlugin
+    {
+        /// Executes the plugin's functionality.
+        void Execute();
+    }
+}
